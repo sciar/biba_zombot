8f3a6df58caca9db900a458a3686b21f6b7d6416
Robin Pan <robin@playbiba.com>
2015-09-21 13:33:33 -0700

    Include Vuforia.

---
+++ Assets/BibaFramework/BibaTag/Model/BibaTag.cs
@@ -0,0 +1,21 @@
+﻿namespace BibaFramework.BibaTag
+{
+    public enum BibaTag 
+    {
+        //Equipments
+        equipment_bridge,
+        equipment_tube,
+        equipment_overhang,
+        equipment_climber,
+        equipment_swing,
+        equipment_slide,
+
+        //Satellites
+        satellite_red,
+        satellite_orange,
+        satellite_yellow,
+        satellite_green,
+        satellite_purple,
+        satellite_blue,
+    }
+}
