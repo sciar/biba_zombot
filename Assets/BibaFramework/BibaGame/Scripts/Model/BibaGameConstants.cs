f3ae0c6faf1cb48f8e8b688a7ac0942b479928de
Robin Pan <robin@playbiba.com>
2015-11-23 09:52:13 -0800

    Change Biba about URL.

--- Assets/BibaFramework/BibaGame/Scripts/Model/BibaGameConstants.cs
+++ Assets/BibaFramework/BibaGame/Scripts/Model/BibaGameConstants.cs
@@ -2,7 +2,7 @@ namespace BibaFramework.BibaGame
 {
     public class BibaGameConstants
     {
-        public const string BIBA_PRIVACY_POLICY_URL = "http://www.playbiba.com/privacy-policy";
+        public const string BIBA_WEBSITE_URL = "http://www.playbiba.com";
 
         public const int ONE_DAY_IN_SECONDS = 86400; //1 day
         public const int ONE_HOUR_IN_SECONDS = 3600; //1 hr
