70379807fddb123c906677c3068ba631ddaf4d32
Chema Leon <blue.joeco@gmail.com>
2016-04-04 15:06:05 -0700

    Added BibaLocalizedServiceView as an accessor for the LocalizationService to use it on scripts. Binded it on the BaseBibaMenuContext.

---
+++ Assets/BibaFramework/BibaMenu/View/BibaLocalizedServiceMediator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.mediation.impl;
+using BibaFramework.BibaGame;
+
+public class BibaLocalizedServiceMediator : Mediator {
+
+	[Inject]
+	public LocalizationService localizationService { get; set; }
+
+	[Inject]
+	public BibaLocalizedServiceView view { get; set; }
+
+	public override void OnRegister () {
+		view.localizationService = localizationService;
+	}
+
+	public override void OnRemove() {
+		
+	}
+}
