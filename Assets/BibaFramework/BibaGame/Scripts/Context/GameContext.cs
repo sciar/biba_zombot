0763a345a92fecc27e089a1e80bbab83868edb9d
Robin Pan <robin@playbiba.com>
2016-04-26 14:07:09 -0700

    Mock game model if starts in the GameScene in Editor without SelectedEquipments

--- Assets/BibaFramework/BibaGame/Scripts/Context/GameContext.cs
+++ Assets/BibaFramework/BibaGame/Scripts/Context/GameContext.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
-using BibaFramework.BibaMenu;
+﻿using BibaFramework.BibaMenu;
 using strange.extensions.context.api;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace BibaFramework.BibaGame
 {
@@ -16,6 +18,15 @@ namespace BibaFramework.BibaGame
         
         protected override void BindModels ()
         {
+			#if UNITY_EDITOR
+			var sessionModel = injectionBinder.GetInstance<BibaSessionModel>();
+			if(sessionModel.SelectedEquipments.Count == 0)
+			{
+				sessionModel.SelectedEquipments.Add(new BibaEquipment(BibaEquipmentType.bridge));
+				sessionModel.SelectedEquipments.Add(new BibaEquipment(BibaEquipmentType.climber));
+				sessionModel.SelectedEquipments.Add(new BibaEquipment(BibaEquipmentType.overhang));
+			}
+			#endif
         }
         
         protected override void BindServices ()
