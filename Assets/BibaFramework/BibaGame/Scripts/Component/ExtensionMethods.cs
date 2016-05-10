32388c67bba19c28081607d63aef82454818744d
Chema Leon <blue.joeco@gmail.com>
2016-05-09 17:04:15 -0700

    Added particle management extension methods for easier handling of Unity 5.3 ParticleSystem structs.

--- Assets/BibaFramework/BibaGame/Scripts/Component/ExtensionMethods.cs
+++ Assets/BibaFramework/BibaGame/Scripts/Component/ExtensionMethods.cs
@@ -10,4 +10,23 @@ public static class ExtensionMethods {
 		canvasGroup.blocksRaycasts = value;
 		canvasGroup.interactable = value;
 	}
+
+	public static void EnableEmission(this ParticleSystem particleSystem, bool enabled)
+	{
+		var emission = particleSystem.emission;
+		emission.enabled = enabled;
+	}
+
+	public static float GetEmissionRate(this ParticleSystem particleSystem)
+	{
+		return particleSystem.emission.rate.constantMax;
+	}
+
+	public static void SetEmissionRate(this ParticleSystem particleSystem, float emissionRate)
+	{
+		var emission = particleSystem.emission;
+		var rate = emission.rate;
+		rate.constantMax = emissionRate;
+		emission.rate = rate;
+	}
 }
