54bf9bda1a86b169eddea6dd19dddd0cced21968
Robin Pan <robin@playbiba.com>
2016-07-06 12:01:34 -0700

    Fix compile with the animator refactor.

--- Assets/BibaFramework/BibaMenu/Command/SetupEditorDebugSceneCommand.cs
+++ Assets/BibaFramework/BibaMenu/Command/SetupEditorDebugSceneCommand.cs
@@ -1,17 +1,18 @@
 using strange.extensions.command.impl;
 using UnityEngine;
+using UnityEngine.Experimental.Director;
 
 namespace BibaFramework.BibaMenu
 {
     public class SetupEditorDebugSceneCommand : Command
     {
         [Inject(BibaMenuConstants.BIBA_STATE_MACHINE)]
-        public Animator StateMachine { get; set; }
+		public IAnimatorControllerPlayable StateMachine { get; set; }
 
         public override void Execute ()
         {
             #if UNITY_EDITOR
-            StateMachine.CrossFade(Application.loadedLevelName, 0);
+			((Animator)StateMachine).CrossFade(Application.loadedLevelName, 0);
             #endif
         }
     }
