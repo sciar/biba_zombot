1ac4440f25728b76495ba82f86d77d9ba6f4340e
Robin Pan <robin@playbiba.com>
2016-08-16 15:58:32 -0700

    BaseObjectMenuState to allow animation.

--- Assets/BibaFramework/BibaMenu/View/BaseObjectMenuStateView.cs
+++ Assets/BibaFramework/BibaMenu/View/BaseObjectMenuStateView.cs
@@ -1,6 +1,7 @@
+using System;
+using System.Collections;
 using strange.extensions.mediation.impl;
 using UnityEngine;
-using System;
 
 namespace BibaFramework.BibaMenu
 {
@@ -8,10 +9,17 @@ namespace BibaFramework.BibaMenu
 	{
 		public CanvasGroup CanvasGroup { get { return GetComponent<CanvasGroup>(); } }
 
+		public Animator Animator { get { return GetComponent<Animator> (); } }
+
 		public virtual void AnimateEntry()
 		{
 			gameObject.SetActive(true);
-			if(CanvasGroup != null)
+			if (Animator != null && Animator.runtimeAnimatorController != null) 
+			{
+				StartCoroutine (PlayAnimation (BibaMenuConstants.BIBA_MENU_ENTRY_ANIMATION_TRIGGER, 
+												BibaMenuConstants.BIBA_MENU_ENTRY_ANIMATION_STATE));
+			} 
+			else if (CanvasGroup != null) 
 			{
 				CanvasGroup.alpha = 0;
 				CanvasGroup.FadeAlphaTo(1, .5f);
@@ -20,13 +28,35 @@ namespace BibaFramework.BibaMenu
 
 		public virtual void AnimateExit()
 		{
-			if(CanvasGroup != null)
+			if (Animator != null && Animator.runtimeAnimatorController != null) 
+			{
+				StartCoroutine (PlayAnimation (BibaMenuConstants.BIBA_MENU_EXIT_ANIMATION_TRIGGER,
+												BibaMenuConstants.BIBA_MENU_EXIT_ANIMATION_STATE,
+												() => gameObject.SetActive (false)));
+			}
+			else if(CanvasGroup != null)
 			{	
 				CanvasGroup.FadeAlphaTo(0, .5f, 0, () => gameObject.SetActive(false));
 			}
 			else
 			{
 				gameObject.SetActive(false);
+			}
+		}
+
+		IEnumerator PlayAnimation(string animationTrigger, string animationStateName, Action onComplete = null)
+		{
+			Animator.SetTrigger(animationTrigger);
+
+			yield return new WaitUntil(() => Animator.GetCurrentAnimatorStateInfo(0).IsName(animationStateName));
+			while (Animator.GetCurrentAnimatorStateInfo(0).IsName(animationStateName))
+			{
+				yield return new WaitForEndOfFrame();
+			}
+
+			if (onComplete != null) 
+			{
+				onComplete ();
 			}
 		}
 	} 
