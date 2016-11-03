dae422d489fb408f06204f710d359e4280495cbe
Matt Rathbun <mattrathbun@Matts-MacBook-Air.local>
2016-11-03 12:31:38 -0700

    - animated introduction screen - partial work complete on prize screen

--- Assets/MDev/Scripts/ThemeSelector.cs
+++ Assets/MDev/Scripts/ThemeSelector.cs
@@ -19,12 +19,19 @@ public class ThemeSelector : MonoBehaviour {
 
     public BibaCanvasGroup bibaCanvasGroup;
 
+	private bool pictureRoulette;
+
     void OnEnable()
     {
         if (GameManager.Instance.victor == "Zombies")
             maxThemeCount = zombieThemes.Length;
         else
             maxThemeCount = survivorThemes.Length;
+
+		if (anim.GetBool ("Custom", true)) {
+			// If the boolean is triggered for custom prizes make sure we don't run
+		} else
+			pictureRoulette = true; // If we don't use custom prizes default to images
 
         themeCounter = 0; // Reset theme counter so we always start at 0
         rotationTotal = 0; // Reset every time we start back up
@@ -38,32 +45,34 @@ public class ThemeSelector : MonoBehaviour {
         
 	
 	void Update () {
-        imageSwapTimerMax += 0.002f; // Slowly increment this to slow down the image swaps
+		if (pictureRoulette) {
+			imageSwapTimerMax += 0.002f; // Slowly increment this to slow down the image swaps
 
-        if (imageSwapTimer > 0)
-            imageSwapTimer-= Time.deltaTime; // Every second we go down 1
-        else
-        {
-            if (GameManager.Instance.victor == "robbers")
-                GetComponent<Image>().sprite = zombieThemes[themeCounter];
-            else
-                GetComponent<Image>().sprite = survivorThemes[themeCounter];
-            imageSwapTimer = imageSwapTimerMax;
-            rotationTotal++;
-            AudioManager.Instance.PlaySFX(AudioManager.Instance.rouletteTick);
+			if (imageSwapTimer > 0)
+				imageSwapTimer -= Time.deltaTime; // Every second we go down 1
+        else {
+				if (GameManager.Instance.victor == "robbers")
+					GetComponent<Image> ().sprite = zombieThemes [themeCounter];
+				else
+					GetComponent<Image> ().sprite = survivorThemes [themeCounter];
+				imageSwapTimer = imageSwapTimerMax;
+				rotationTotal++;
+				AudioManager.Instance.PlaySFX (AudioManager.Instance.rouletteTick);
 
-            // If theme counter is at the max theme count increment else reset
-            if (themeCounter < maxThemeCount-1) 
-                themeCounter++;
-            else
-                themeCounter = 0;
-        }
+				// If theme counter is at the max theme count increment else reset
+				if (themeCounter < maxThemeCount - 1)
+					themeCounter++;
+				else
+					themeCounter = 0;
+			}
 
-        if (rotationTotal >= rotationMax && triggerOnce == false)
-        {
-            triggerOnce = true;
-            anim.SetTrigger("Next");
-        }
+			if (rotationTotal >= rotationMax && triggerOnce == false) {
+				triggerOnce = true;
+				anim.SetTrigger ("Next");
+			}
 
+		} else {
+			// Do custom prize rotation
+		}
 	}
 }
